aa7698924ec20b096c9e8ca3291f7d59ec16744a
Dima Kopfmann <dmitry.golovinov@jetbrains.com>
2026-01-14 22:22:02 +0100

    Introduce unsafe query

--- security/case1/src/main/java/com/example/zeroday/UserRepository.java
+++ security/case1/src/main/java/com/example/zeroday/UserRepository.java
@@ -2,15 +2,14 @@ package com.example.zeroday;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class UserRepository {
   public String findUserByName(String nameParam) {
-    String sql = "SELECT * FROM users WHERE name = ?";
+    String sql = "SELECT * FROM users WHERE name = '" + nameParam + "'";
     try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test");
-         PreparedStatement statement = connection.prepareStatement(sql)) {
-      statement.setString(1, nameParam);
-      statement.execute();
+         Statement statement = connection.createStatement()) {
+      statement.execute(sql);
       return sql;
     } catch (Exception e) {
       throw new RuntimeException(e);
